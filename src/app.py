83144b4628c69e868e1c38379e6cb68aa912d4e6
Miguel Alberto Ordoñez <miguelssj5040@gmail.com>
2023-12-31 02:44:26 +0000

    Proyecto API REST

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+from ast import excepthandler
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
@@ -8,8 +9,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Characters, Planets, Vehicles, Favorites
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -31,21 +31,384 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+
+# ----------------------------------------------------------------------------------------------------------------------------
+
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# Obtener y crear usuarios
+@app.route("/user", methods=["GET", "POST"])
+def handle_user():
+    if request.method == "POST":
+        data = request.json  # Informacion recibida en el body de la peticion
 
-    return jsonify(response_body), 200
+        # Verificar los datos enviados en request
+        if (data.get("name") is None or 
+            data.get("lastName") is None or 
+            data.get("email") is None):
+            return jsonify({
+                "message": "Falta informacion, verifica tu peticion."
+            }), 400
+        
+        # Verificar que el email sea unico
+        email_exist = User.query.filter_by(email=data["email"]).one_or_none()
+        if email_exist:
+            return jsonify({
+                "message": "Email ya esta en uso"
+            })
+        
+        # Crear el usuario
+        new_user = User(name=data["name"], last_name=data["lastName"], email=data["email"])
 
-# this only runs if `$ python src/app.py` is executed
+        # Guardar en la base de datos
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+        
+        except Exception as error:
+            db.session.rollback()
+            print(error)
+            return jsonify({
+                "message": "Algo salio mal con la base de datos, intenta nuevamente"
+            }), 500
+        
+        # Retornar repuesta positiva al usuario
+        new_user_id = new_user.id
+        return jsonify({
+            "message": "Usuario creado con exito",
+            "user_id": new_user_id
+        }), 201
+    
+    if request.method == "GET":
+        users = User.query.all()
+        user_list = [{"id": user.id, "email": user.email} for user in users]
+        return jsonify({'users': user_list})
+
+
+# Solicitar personajes
+@app.route("/characters", methods=["GET"])
+def display_characters():
+    characters = Characters.query.all()
+    characters_list = [{"id": character.id, "name": character.name} for character in characters]
+    return jsonify({'Characters': characters_list})
+
+
+# Solicitar planetas
+@app.route("/planets", methods=["GET"])
+def display_planets():
+    planets = Planets.query.all()
+    planets_list = [{"id": planet.id, "name": planet.name} for planet in planets]
+    return jsonify({'Planets': planets_list})    
+
+
+# Solicitar vehiculos
+@app.route("/vehicles", methods=["GET"])
+def display_vehicles():
+    vehicles = Vehicles.query.all()
+    vehicles_list = [{"id": vechicle.id, "name": vechicle.name} for vechicle in vehicles]
+    return jsonify({'Vehicles': vehicles_list})
+
+
+# Solicitar personajes por el id
+@app.route("/characters/<int:id>", methods=["GET"])
+def display_character_info(id):
+    character = Characters.query.get(id)
+    
+    if character is None:
+        return jsonify({
+            "message": "No existe un personaje con esa id"
+        }), 404
+    
+    character_properties = [{"name": character.name, "birth_year": character.birth_year, "gender": character.gender, "height": character.height, "skin_color": character.skin_color, "eye_color": character.eye_color}]
+    return jsonify({
+        "message": "ok",
+        "Properties": character_properties
+    })
+
+
+# Solicitar planetas por el id
+@app.route("/planets/<int:id>", methods=["GET"])
+def display_planet_info(id):
+    planet = Planets.query.get(id)
+
+    if planet is None:
+        return jsonify({
+            "message": "No existe un planeta con esa id"
+        }), 404
+    planet_properties = [{"name": planet.name, "diameter": planet.diameter, "gravity": planet.gravity, "population": planet.population, "climate": planet.climate, "terrain": planet.terrain}]
+    return jsonify({
+        "message": "ok",
+        "properties": planet_properties
+    })
+
+
+# Solicitar vehiculos por el id
+@app.route("/vehicles/<int:id>", methods=["GET"])
+def display_vehicle_info(id):
+    vehicle = Vehicles.query.get(id)
+
+    if vehicle is None:
+        return jsonify({
+            "message": "No existe un vehiculo con esa id"
+        }), 404
+    
+    vehicle_properties = [{"name": vehicle.name, "model": vehicle.model, "vehicle_class": vehicle.vehicle_class, "passengers": vehicle.passengers, "manufacturer": vehicle.manufacturer, "length": vehicle.length}]
+    return jsonify({
+        "message": "ok",
+        "properties": vehicle_properties
+    })
+
+
+# Solicitar lista de favoritos con el id del usuario
+@app.route("/user/<int:user_id>/favorites", methods=["GET"])
+def display_favorites(user_id):
+    user = User.query.get(user_id)
+
+    if user is None:
+        return jsonify({
+            "message": "No existe un usuario con esa id"
+        }), 404
+
+    favorites_list = []
+    for favorite in user.favorites:
+        if favorite.characters:
+            favorites_list.append({"id": favorite.characters.id, "name": favorite.characters.name})
+        elif favorite.planets:
+            favorites_list.append({"id": favorite.planets.id, "name": favorite.planets.name})
+        elif favorite.vehicles:
+            favorites_list.append({"id": favorite.vehicles.id, "name": favorite.vehicles.name})
+
+    return jsonify({
+        "favorites": favorites_list
+    })   
+
+
+# Agregar personaje favorito a usuario
+@app.route("/user/<int:user_id>/favorite/character/<int:character_id>", methods=["POST"])
+def add_favorite_character(user_id, character_id):
+
+    # Verificar si el favorito ya existe para el usuario
+    existing_favorite = Favorites.query.filter_by(
+        user_id=user_id,
+        id_characters=character_id
+    ).first()
+
+    if existing_favorite:
+        return jsonify({
+            "message": "Este personaje ya está en la lista de favoritos del usuario"
+        }), 400
+
+    # Creacion del favorito
+    new_favorite_character = Favorites(user_id=user_id, id_characters=character_id)
+
+    # Guardar en la base de datos
+    try:
+        db.session.add(new_favorite_character)
+        db.session.commit()
+
+    except Exception as error:
+        db.session.rollback()
+        print(error)
+        return jsonify({
+            "message": "Algo salio mal con la base de datos, intenta nuevamente"
+        }), 500
+
+    # Retornar repuesta positiva al usuario
+    return jsonify({
+        "message": "Agregado con exito"
+    }), 201
+
+
+# Agregar planeta favorito a usuario
+@app.route("/user/<int:user_id>/favorite/planet/<int:planet_id>", methods=["POST"])
+def add_favorite_planet(user_id, planet_id):
+
+    # Verificar si el favorito ya existe para el usuario
+    existing_favorite = Favorites.query.filter_by(
+        user_id=user_id,
+        id_planets=planet_id
+    ).first()
+
+    if existing_favorite:
+        return jsonify({
+            "message": "Este planeta ya está en la lista de favoritos del usuario"
+        }), 400
+
+    # Creacion del favorito
+    new_favorite_planet = Favorites(user_id=user_id, id_planets=planet_id)
+
+    # Guardar en la base de datos
+    try:
+        db.session.add(new_favorite_planet)
+        db.session.commit()
+        
+    except Exception as error:
+        db.session.rollback()
+        print(error)
+        return jsonify({
+            "message": "Algo salio mal con la base de datos, intenta nuevamente"
+        }), 500
+        
+    # Retornar repuesta positiva al usuario
+    return jsonify({
+        "message": "Agregado con exito"
+    }), 201
+
+
+# Agregar vehiculo favorito a usuario
+@app.route("/user/<int:user_id>/favorite/vehicle/<int:vehicle_id>", methods=["POST"])
+def add_favorite_vehicle(user_id, vehicle_id):
+
+    # Verificar si el favorito ya existe para el usuario
+    existing_favorite = Favorites.query.filter_by(
+        user_id=user_id,
+        id_vehicles=vehicle_id
+    ).first()
+
+    if existing_favorite:
+        return jsonify({
+            "message": "Este vehiculo ya está en la lista de favoritos del usuario"
+        }), 400
+
+    # Creacion del favorito
+    new_favorite_vehicle = Favorites(user_id=user_id, id_vehicles=vehicle_id)
+
+    # Guardar en la base de datos
+    try:
+        db.session.add(new_favorite_vehicle)
+        db.session.commit()
+        
+    except Exception as error:
+        db.session.rollback()
+        print(error)
+        return jsonify({
+            "message": "Algo salio mal con la base de datos, intenta nuevamente"
+        }), 500
+        
+    # Retornar repuesta positiva al usuario
+    return jsonify({
+        "message": "Agregado con exito"
+    }), 201
+
+
+# Eliminar personaje favorito de un usuario
+@app.route("/user/<int:user_id>/favorite/character/<int:character_id>", methods=["DELETE"])
+def delete_favorite_character(user_id, character_id):
+
+    # Verificar si el usuario existe
+    user = User.query.get(user_id)
+
+    if user is None:
+        return jsonify({
+            "message": "No existe un usuario con esa id."
+        }), 404
+
+    # Extraer el favorito de la tabla favorites
+    favorite_to_delete = Favorites.query.filter_by(id_characters=character_id, user_id=user_id).first()
+
+    # Verificar si el favorito existe en los favoritos del Usuario
+    if favorite_to_delete is None:
+        return jsonify({
+            "message": "No existe un favorito con esa id para el usuario."
+        }), 404
+
+    # Eliminar el favorito del usuario
+    try:
+        db.session.delete(favorite_to_delete)
+        db.session.commit()
+
+        return jsonify({
+            "message": "Favorito eliminado con éxito."
+        })
+
+    except Exception as error:
+        db.session.rollback()
+        print(error)
+        return jsonify({
+            "message": "Algo salió mal al eliminar el favorito, intenta nuevamente."
+        }), 500
+
+
+# Eliminar planeta favorito de un usuario
+@app.route("/user/<int:user_id>/favorite/planet/<int:planet_id>", methods=["DELETE"])
+def delete_favorite_planet(user_id, planet_id):
+
+    # Verificar si el usuario existe
+    user = User.query.get(user_id)
+
+    if user is None:
+        return jsonify({
+            "message": "No existe un usuario con esa id."
+        }), 404
+
+    # Extraer el favorito de la tabla favorites
+    favorite_to_delete = Favorites.query.filter_by(id_planets=planet_id, user_id=user_id).first()
+
+    # Verificar si el favorito existe en los favoritos del Usuario
+    if favorite_to_delete is None:
+        return jsonify({
+            "message": "No existe un favorito con esa id para el usuario."
+        }), 404
+
+    # Eliminar el favorito del usuario
+    try:
+        db.session.delete(favorite_to_delete)
+        db.session.commit()
+
+        return jsonify({
+            "message": "Favorito eliminado con éxito."
+        })
+
+    except Exception as error:
+        db.session.rollback()
+        print(error)
+        return jsonify({
+            "message": "Algo salió mal al eliminar el favorito, intenta nuevamente."
+        }), 500
+
+
+# Eliminar vehiculo favorito de un usuario
+@app.route("/user/<int:user_id>/favorite/vehicle/<int:vehicle_id>", methods=["DELETE"])
+def delete_favorite_vehicle(user_id, vehicle_id):
+
+    # Verificar si el usuario existe
+    user = User.query.get(user_id)
+
+    if user is None:
+        return jsonify({
+            "message": "No existe un usuario con esa id."
+        }), 404
+
+    # Extraer el favorito de la tabla favorites
+    favorite_to_delete = Favorites.query.filter_by(id_vehicles=vehicle_id, user_id=user_id).first()
+
+    # Verificar si el favorito existe en los favoritos del Usuario
+    if favorite_to_delete is None:
+        return jsonify({
+            "message": "No existe un favorito con esa id para el usuario."
+        }), 404
+
+    # Eliminar el favorito del usuario
+    try:
+        db.session.delete(favorite_to_delete)
+        db.session.commit()
+
+        return jsonify({
+            "message": "Favorito eliminado con éxito."
+        })
+
+    except Exception as error:
+        db.session.rollback()
+        print(error)
+        return jsonify({
+            "message": "Algo salió mal al eliminar el favorito, intenta nuevamente."
+        }), 500
+
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -2,18 +2,104 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model):  # Parent
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    last_name = db.Column(db.String(250), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship("Favorites", back_populates="user")
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.name
+
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "email": self.email
+        }
+
+
+class Favorites(db.Model):  # Child
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship("User", back_populates="favorites")
+
+    id_characters = db.Column(db.Integer, db.ForeignKey("characters.id"))
+    characters = db.relationship("Characters", back_populates="favorites")
+
+    id_planets = db.Column(db.Integer, db.ForeignKey("planets.id"))
+    planets = db.relationship("Planets", back_populates="favorites")
+
+    id_vehicles = db.Column(db.Integer, db.ForeignKey("vehicles.id"))
+    vehicles = db.relationship("Vehicles", back_populates="favorites")
+
+ 
+class Characters(db.Model):  # Parent
+    id = db.Column(db.Integer, primary_key=True)
+    favorites = db.relationship("Favorites", back_populates="characters")
+
+    name = db.Column(db.String(200))
+    birth_year = db.Column(db.Integer)
+    gender = db.Column(db.String(50))
+    height = db.Column(db.Integer)
+    skin_color = db.Column(db.String(100))
+    eye_color = db.Column(db.String(100))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "height": self.height,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color
+        }
+
+
+class Planets(db.Model):  # Parent
+    id = db.Column(db.Integer, primary_key=True)
+    favorites = db.relationship("Favorites", back_populates="planets")
+
+    name = db.Column(db.String(200))
+    diameter = db.Column(db.Integer)
+    gravity = db.Column(db.String(100))
+    population = db.Column(db.Integer)
+    climate = db.Column(db.String(100))
+    terrain = db.Column(db.String(100))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain
+        }
+
+
+class Vehicles(db.Model):  # Parent
+    id = db.Column(db.Integer, primary_key=True)
+    favorites = db.relationship("Favorites", back_populates="vehicles")
+
+    name = db.Column(db.String(200))
+    model = db.Column(db.String(100))
+    vehicle_class = db.Column(db.String(100))
+    passengers = db.Column(db.Integer)
+    manufacturer = db.Column(db.String(100))
+    length = db.Column(db.Integer)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "vehicle_class": self.vehicle_class,
+            "passengers": self.passengers,
+            "manufacturer": self.manufacturer,
+            "length": self.length
         }
